3bb0b937dc05bfc22a4d21e8300131cbda5e8291
Li Jian <lijian@ooclab.com>
2016-06-23 11:41:05 +0800

    app/account:开始admin接口开发

--- app/account/urls.py
+++ app/account/urls.py
@@ -3,6 +3,7 @@
 from tornado.web import url
 
 from .console import views as console_views
+from .admin import views as admin_views
 
 
 handlers = [
@@ -10,4 +11,11 @@ handlers = [
     # console
     url(r'/console/account/profile', console_views.MyProfile),
     url(r'/console/account/password', console_views.PasswordReset),
+
+    # console
+    url(r'/admin/account/user', admin_views.UsersHandler),
+    url(r'/admin/account/user/([0-9]+)', admin_views.UserHandler),
+    url(r'/admin/account/user/([0-9]+)/profile', admin_views.UserProfile),
+    url(r'/admin/account/user/([0-9]+)/password', admin_views.UserPasswordReset),
+
 ]
