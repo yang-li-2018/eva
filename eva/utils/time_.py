268df94d7f10bb1a21fde80447ede35c5bfb93c4
Li Jian <lijian@ooclab.com>
2017-05-06 10:55:43 +0800

    增加rfc3339与datetime的相互转换方法

--- eva/utils/time_.py
+++ eva/utils/time_.py
@@ -88,3 +88,25 @@ def rfc3339_string(t):
     '''转化 datetime 为 rfc3339 格式字符串'''
     if isinstance(t, datetime.datetime):
         return t.isoformat('T') + 'Z'
+
+
+def utc_rfc3339_string(dt):
+    '''转化 datetime(UTC) 为 rfc3339 格式字符串'''
+
+    if isinstance(dt, datetime.datetime):
+        return dt.isoformat('T') + 'Z'
+
+    return ''
+
+
+def utc_rfc3339_parse(s):
+    '''转化 rfc3339 (UTC) 格式字符串为 datetime'''
+
+    if not s:
+        return
+    if s[-1].upper() != 'Z':
+        return
+    if '.' in s:
+        return datetime.datetime.strptime(s.rstrip('Zz'), '%Y-%m-%dT%H:%M:%S.%f')
+    else:
+        return datetime.datetime.strptime(s.rstrip('Zz'), '%Y-%m-%dT%H:%M:%S')
